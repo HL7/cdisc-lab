5a87468064d5b2dd645b98614ac59c45b0540901
CDenney <im4infomgt@gmail.com>
2020-02-28 10:19:58 -0500

    Testing new IGPublisher

--- _genonce.sh
+++ _genonce.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
-java -jar org.hl7.fhir.igpublisher.jar -ig ig.json
+#java -jar org.hl7.fhir.igpublisher.jar -ig ig.json
+
+Java -jar org.hl7.fhir.publisher.jar -ig ig.json
